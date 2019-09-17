3b0171f717ccde437c6d73dfff074d1db1a697a8
matx17 <43521617+matx17@users.noreply.github.com>
2019-09-17 16:34:37 +0200

    git commit -m "sub test cases"

--- test.py
+++ test.py
@@ -18,6 +18,19 @@ class MyTestCase(unittest.TestCase):
             self.assertMultiLineEqual('1.66666666667', rv.data)
         def test_addneg(self):
             rv =  self.app.get('/add?A=2.3&B=-3.3')
-            self.assertMultiLineEqual('-1.0', rv.data)        
+            self.assertMultiLineEqual('-1.0', rv.data)  
+                
+        def test_subint(self):
+            rv =  self.app.get('/sub?A=2&B=5')
+            self.assertMultiLineEqual('-3', rv.data)
+        def test_subfloat(self):
+            rv =  self.app.get('/sub?A=2.3&B=3.3')
+            self.assertMultiLineEqual('-1.0', rv.data)
+        def test_subfrac(self):
+            rv =  self.app.get('/sub?A=2/3&B=3/3')
+            self.assertMultiLineEqual('-0.333333333333', rv.data)
+        def test_subneg(self):
+            rv =  self.app.get('/sub?A=2.3&B=-3.3')
+            self.assertMultiLineEqual('5.6', rv.data)
                 
                 
